2477571f41ec6ab384917bbf549f6cba47c8a0a1
Pedro Campelo Matheus <pedrohcmatheus@gmail.com>
2021-09-25 13:21:21 -0300

    linted code

--- src/server.js
+++ src/server.js
@@ -1,97 +1,83 @@
 const http = require('http');
-const config = require('../config')
-const { bodyParser, queryParser, URLFormatter } = require('./helpers/parsers')
+const config = require('./config');
+const { bodyParser, queryParser, URLFormatter } = require('./helpers/parsers');
 const headers = require('./helpers/headers');
 const router = require('./router');
 const logger = require('./helpers/logger');
-const { requestHeaderOptions } = require('../config');
 
 const requestListener = async (req, res) => {
-  
-  headers.requestHeaderOptions(req);
+	headers.requestHeaderOptions(req);
 
-  const requestURL = await URLFormatter(config.options, req);
-  
-  req.pathname = requestURL.pathname.toString()
-  if (req.pathname === '/favicon.ico') return res.end()
+	const requestURL = URLFormatter(config.options, req);
 
-  req.address = req.socket.localAddress + ':' + req.socket.localPort
-  req.method = req.method
-  req.query = queryParser(requestURL);
+	req.pathname = requestURL.pathname.toString();
+	if (req.pathname === '/favicon.ico') return res.end();
 
+	req.address = `${req.socket.localAddress}:${req.socket.localPort}`;
+	req.query = queryParser(requestURL);
 
-  // console.log('\n--------------------------------------------------------------------------')
-  // console.log('New Request Incoming!')
-    
-  
-  // TODO : EXTRACT API TOKEN FROM URL
-  headers.responseHeaderOptions('allowed', res);
+	// console.log('\n--------------------------------------------------------------------------')
+	// console.log('New Request Incoming!')
 
-  let { pathname } = requestURL
-  let id = null;
-  let username = null;
-  
-  const splitEndpoint = pathname.split('/').filter(Boolean); // WOW
-  
-  
-  if (splitEndpoint.length > 1) {
-    if (Number(splitEndpoint[1])) {
-      pathname = `/${splitEndpoint[0]}/:id`
-      id = Number(splitEndpoint[1]);
-    }
-    else {
-      pathname = `/${splitEndpoint[0]}/:name`
-      username = splitEndpoint[1].toLowerCase();
-    }
-  }
-  
-  res.send = (statusCode, body) => {
-    res.writeHead(statusCode, { 'Content-Type': 'application/json'});
-    res.end(JSON.stringify(body));    
-  }
-  
-  const route = router.find((routeObject) => (
-    routeObject.endpoint === pathname && routeObject.method === req.method
-  ));
+	// TODO : EXTRACT API TOKEN FROM URL
+	headers.responseHeaderOptions('allowed', res);
 
-  if (route) {
-      
-    req.params = { id, username }
+	let { pathname } = requestURL;
+	let id = null;
+	let username = null;
 
-    res.send = (statusCode, body) => {
-      res.writeHead(statusCode, { 'Content-Type': 'application/json'});
-      res.end(JSON.stringify(body));    
-    }
+	const splitEndpoint = pathname.split('/').filter(Boolean); // WOW
 
-    if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method)) {
+	if (splitEndpoint.length > 1) {
+		if (Number(splitEndpoint[1])) {
+			pathname = `/${splitEndpoint[0]}/:id`;
+			id = Number(splitEndpoint[1]);
+		} else {
+			pathname = `/${splitEndpoint[0]}/:name`;
+			username = splitEndpoint[1].toLowerCase();
+		}
+	}
 
-      bodyParser(req, () => { route.handler(req, res)})
+	res.send = (statusCode, body) => {
+		res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify(body));
+	};
 
-    } else {
+	const route = router.find((routeObject) => (
+		routeObject.endpoint === pathname && routeObject.method === req.method
+	));
 
-      route.handler(req, res);
-    }
+	if (route) {
+		req.params = { id, username };
 
+		res.send = (statusCode, body) => {
+			res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+			res.end(JSON.stringify(body));
+		};
 
-  } else {
+		if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method)) {
+			bodyParser(req, () => route.handler(req, res));
+		} else {
+			route.handler(req, res);
+		}
+	} else {
+		res.writeHead(404, { 'Content-Type': 'text/html' });
+		res.end(`Cannot ${req.method} ${req.url}`);
+	}
 
-  res.writeHead(404, { 'Content-Type': 'text/html'});
-  res.end(`Cannot ${req.method} ${req.url}`);
+	// const response = await router(req, userSocket, res);
 
-  }
+	// TODO separate status code from response
+	// TODO: Connect use to database
 
-  // const response = await router(req, userSocket, res);
+	// res.write(response);
+	// TODO : Fs. writeFile to logs.json
 
-  // TODO separate status code from response
-  // TODO: Connect use to database
+	logger.saveLog();
 
-
-  // res.write(response);
-  // TODO : Fs. writeFile to logs.json
-  
-  logger.saveLog()
-}
+	return undefined;
+};
 
 const server = http.createServer(requestListener);
 
-module.exports = server, requestListener;
+module.exports = server;
